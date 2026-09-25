e953fa5e2c643dd5b0b2aa6db0c051cce21e507c
Vishnu_Anilkumar <25472713+vishnuverse@users.noreply.github.com>
2026-09-26 05:20:09 +0530

    fix(time): use local date.today() instead of UTC for naturalday (#4)

--- src/humanize/time.py
+++ src/humanize/time.py
@@ -341,7 +341,7 @@ def naturalday(value: dt.date | dt.datetime, format: str = "%b %d") -> str:
         if isinstance(value, dt.datetime) and value.tzinfo is not None:
             today = dt.datetime.now(value.tzinfo).date()
         else:
-            today = dt.datetime.now(dt.timezone.utc).date()
+            today = dt.date.today()
         value = dt.date(value.year, value.month, value.day)
     except AttributeError:
         # Passed value wasn't date-ish
